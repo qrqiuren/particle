10d23618dae34e78b608c032de56d0a331582a94
秋纫 <dugucloud@qq.com>
2016-05-14 20:54:05 +0800

    Create DOAArray
    Create `DOAArray` as the base class of sensor arrays.

---
+++ doaarray.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 13 09:50:09 2016
+
+@author: duguc
+"""
+
+
+class DOAArray:
+    """
+    Base class of sensor array in DOA problems.
+
+    Reference
+    ---------
+    Zhong, X., Prekumar, A. B., and Madhukumar, A. S., "Particle filtering for
+    acoustic source tracking in impulsive noise with alpha-stable process",
+    IEEE Sensors Journal, Feb. 2013, Vol. 13 No. 2: 589-600.
+    """
+
+    def __init__(self, nsensor, wavelength):
+        """
+        Initialization of DOASteer.
+
+        Parameters
+        ----------
+        nsensor : int
+            Number of sensors.
+        wavelength : float
+            Wavelength of incoming wave.
+        """
+        self.nsensor = nsensor
+        self.wavelength = wavelength
+
+    def steer(self, angle):
+        """
+        Returns the steering vector of a DOA angle.
+
+        You should implement it by every kind of array layout.
+
+        Parameters
+        ----------
+        angle : float
+            DOA angle in [0, pi).
+
+        Returns
+        -------
+        a : (nsensor,) ndarray
+            The steering vector corresponding to `angle`.
+        """
+        raise NotImplementedError

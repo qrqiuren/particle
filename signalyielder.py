2a663483ae673a8b9135c2f3d1cff88bb6c4fe96
秋纫 <dugucloud@qq.com>
2016-05-17 00:19:46 +0800

    Add `SignalYielder`

---
+++ signalyielder.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 16 20:01:31 2016
+
+@author: Huang Hongye <qrqiuren@users.noreply.github.com>
+"""
+
+import numpy as np
+
+from salphas import salphas_cplx
+
+
+class SignalYielder:
+    """
+    Generating and yielding signals.
+
+    See the reference for definition and form of signal.
+
+    Reference
+    ---------
+    Zhong, X., Prekumar, A. B., and Madhukumar, A. S., "Particle filtering for
+    acoustic source tracking in impulsive noise with alpha-stable process",
+    IEEE Sensors Journal, Feb. 2013, Vol. 13 No. 2: 589-600.
+    """
+    def __init__(self, sarr):
+        """
+        Parameters
+        ----------
+        sarr : DOAArray
+            Instance of sensor array.
+        """
+        self.sarr = sarr
+
+    def gen(self, angle):
+        """
+        Generate a snapshot of signal under a given DOA angle.
+
+        The amplitude is 1 and phase follows uniform distribution of
+        [0, 2*pi).
+
+        Parameters
+        ----------
+        angle : float
+            Angle of current snapshot.
+
+        Returns
+        -------
+        y : (nsensors,)
+            A snapshot in DOA `y`. `nsensors` is the number of sensors in
+            `sarr`.
+        """
+        a = self.sarr.steer(angle)
+        phase = np.random.random() * np.pi * 2
+        signal = np.exp(1j * np.pi * phase)
+
+        y = signal * a
+        noise = salphas_cplx(1.5, 1.5, size=y.shape)
+        y += noise
+
+        return y
+
+    def batchgen(self, angles):
+        """
+        Generate snapshots of signal under a given DOA angle.
+
+        The amplitude is 1 and phase follows uniform distribution of
+        [0, 2*pi).
+
+        Parameters
+        ----------
+        angles : (snapshots,)
+            Angle of current snapshot.
+
+        Returns
+        -------
+        y : (nsensors, nsnapshots)
+            A snapshot in DOA `y`. `nsensors` is the number of sensors in
+            `sarr`.
+        """
+        snapshots = angles.size
+
+        phase = np.random.random(snapshots) * np.pi * 2
+        y = salphas_cplx(1.5, 1.5, size=(self.sarr.nsensors, snapshots))
+
+        for i in range(snapshots):
+            a = self.sarr.steer(angles[i])
+            signal = np.exp(1j * np.pi * phase[i])
+            y[:, i] += signal * a
+
+        return y
+
+if __name__ == '__main__':
+    from ulaarray import ULAArray
+
+    ula = ULAArray(5, 3, 1.5)
+    sgen = SignalYielder(ula)
+
+    y = sgen.gen(np.pi / 2)
+    print(y)
+
+    y = sgen.batchgen(np.linspace(0., np.pi, 7))
+    print(y)
